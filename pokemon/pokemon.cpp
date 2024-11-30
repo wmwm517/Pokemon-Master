444059231eb5af311b3221e57a7e8be028b12e5d
wmwm517 <apple051717@gmail.com>
2024-11-30 16:41:24 +0900

    feat: 코드 갈아엎기

--- pokemon/pokemon.cpp
+++ pokemon/pokemon.cpp
@@ -1,186 +1,140 @@
 ﻿#include <iostream>
 #include <vector>
-#include <string>
+#include <cstring>
 #include <iomanip>
+
 using namespace std;
-
-void SelectPokemon();
-void PrintBattle(const Pokemon& p1, const Pokemon& p2);
-void ChooseSkill(Pokemon& a, Pokemon& d);
-string UseSkill(Pokemon& a, Pokemon& d);
-
-class Skill {
-public:
-	string name;
-	string type;
-	int damege;
-	int count;
-
-	Skill(const std::string& name, const std::string& type, int damege, int count)
-		: name(name), type(type), damege(damege), count(count) {}
-};
 
 class Pokemon
 {
 public:
-	virtual void setName(const std::string& name) { this->name = name; }
-	virtual void setHP(int hp) { this->HP = hp; }
-	virtual void setType(const std::string& type) { this->type = type; }
-	virtual void setSkill(const std::string& skill, const std::string& type, int damege, int count) {}
-	vector<Skill> s;
-
-	string getName() const { return name; }
-	int getHP() const { return HP; }
-	string getType() const { return type; }
-
-private:
+	void Skill0(Pokemon& other);
+	void Skill1(Pokemon& other);
+	void Skill2(Pokemon& other);
+	void Skill3(Pokemon& other);
+	bool isLive();
+	bool canTry(int Skillnum);
+	int effect(string skillType, string pokemonType);
+protected:
 	string name;
 	string type;
+	string lastskill;
 	int HP;
+	int Skillcnt[4];
 };
 
-class PokemonBuilder
+bool Pokemon::isLive()
 {
-private:
-	Pokemon p;
-public:
-	PokemonBuilder& setName(const std::string& name) {
-		p.setName(name);
-		return *this;
-	}
-	PokemonBuilder& setHP(int hp) {
-		p.setHP(hp);
-		return *this;
-	}
-	PokemonBuilder& setType(const std::string& type) {
-		p.setType(type);
-		return *this;
-	}
-	PokemonBuilder& setSkill(const std::string& skill, const std::string& type, int damege, int count) {
-		p.s.emplace_back(skill, type, damege, count);
-		return *this;
-	}
-	Pokemon build() {
-		return p;
-	}
-};
-
-std::vector<Pokemon> createPokemons(){
-	std::vector<Pokemon> pokemons;
-
-	pokemons.push_back(PokemonBuilder()
-		.setName("Pikachu")
-		.setHP(35)
-		.setType("Electric")
-		.setSkill("Tackle", "Normal", 4, 5)
-		.setSkill("Grass Knot", "Grass", 8, 5)
-		.setSkill("Thunderbolt", "Electric", 10, 5)
-		.setSkill("Megabolt", "Electric", 15, 3)
-		.build());
-	pokemons.push_back(PokemonBuilder()
-		.setName("Dratini")
-		.setHP(41)
-		.setType("Water")
-		.setSkill("Wrap", "Normal", 4, 10)
-		.setSkill("Aqua Tail", "Water", 3, 5)
-		.setSkill("Water Pulse", "Water", 13, 2)
-		.setSkill("Hyper Beam", "Normal", 20, 1)
-		.build());
-	pokemons.push_back(PokemonBuilder()
-		.setName("Eevee")
-		.setHP(55)
-		.setType("Normal")
-		.setSkill("Tackle", "Normal", 4, 5)
-		.setSkill("Sand Attack", "Ground", 8, 3)
-		.setSkill("Bite", "Normal", 12, 3)
-		.setSkill("Rain Dance", "Water", 15, 1)
-		.build());
-	pokemons.push_back(PokemonBuilder()
-		.setName("Charmander")
-		.setHP(39)
-		.setType("Fire")
-		.setSkill("Tackle", "Normal", 4, 5)
-		.setSkill("Flamethrower", "Fire", 11, 5)
-		.setSkill("Dig", "Ground", 7, 5)
-		.setSkill("Heat Wave", "Fire", 14, 5)
-		.build());
-	pokemons.push_back(PokemonBuilder()
-		.setName("Palkia")
-		.setHP(90)
-		.setType("Water")
-		.setSkill("Hydro Pump", "Water", 12, 10)
-		.setSkill("Earth Power", "Ground", 15, 10)
-		.setSkill("Surt", "Water", 13, 10)
-		.setSkill("Spatial Rend", "Normal", 30, 10)
-		.build());
-
-	return pokemons;
-}
-
-std::pair<Pokemon, Pokemon> SelectPokemon(const std::vector<Pokemon>& pokemons)
-{
-	int p1, p2;
-	cout << "Choose a Pokemon(0~4): ";
-	cin >> p1;
-	cout << "Choose a Pokemon(0~4): ";
-	cin >> p2;
-
-	if (p1 == p2)
+	if (this->HP <= 0)
 	{
-		cout << "You have to choose Pokemons different from each other.";
-		exit(0);
+		return false;
 	}
-	else
+	return true;
+}
+
+bool Pokemon::canTry(int Skillnum)
+{
+	if (this->Skillcnt[Skillnum] <= 0)
 	{
-		return { pokemons[p1], pokemons[p2] };
+		return false;
 	}
+	return true;
 }
 
-void PrintBattle(const Pokemon& p1, const Pokemon& p2)
+int Pokemon::effect(string skillType, string pokemonType)
 {
-	
-	cout << "+-------------------------------------------------------------+" << endl;
-	cout << "| 2024-02 Object-Oriented Programming Pokemon Master          |" << endl;
-	cout << "+------------------------------+------------------------------+" << endl;
-	cout << "| " << p1.getName() << setw(29 - p1.getName().length());
-	cout << "| " << p2.getName() << setw(29 - p2.getName().length()) << "|" << endl;
-	cout << "| Type: " << p1.getType() << setw(23 - p1.getType().length());
-	cout << "| Type: " << p2.getType() << setw(23 - p2.getType().length()) << "|" << endl;
-	cout << "| HP: " << p1.getHP() << setw(25 - to_string(p1.getHP()).length());
-	cout << "| HP: " << p2.getHP() << setw(25 - to_string(p2.getHP()).length()) << "|" << endl;
-	cout << "+------------------------------+------------------------------+" << endl;
-	cout << "| Latest Skill: " << p1.setSkill[0].name << setw(15 - p1.setSkill[0].name);
-	cout << "| Latest Skill: " << p2.setSkill[0].name << setw(15 - p2.setSkill[0].name) << "|" << endl;
-	cout << "| " << UseSkill(p1, p2) << setw(29 - UseSkill(p1, p2));
-	cout << "| " << UseSkill(p2, p1) << setw(29 - UseSkill(p2, p1)) << "|" << endl;
-	cout << "|" << endl;
-	cout << "|" << endl;
-	cout << "+------------------------------+------------------------------+" << endl;
-}
+	//표짜ㅇㅑ됨^^
+	// 0:효과별로, 1:효과짱, 2:효과대박짱
 
-void ChooseSkill(Pokemon& a, Pokemon& d)
-{
-	int skill;
-	cout << "Choose a skill (0~3): ";
-
-}
-
-string UseSkill(Pokemon& a, Pokemon& d)
-{
-	return "It was super effective.";
-	return "It was effective.";
-	return "It was not very effective.";
-	return "";
-
-}
-
-int main() {
-	std::vector<Pokemon> pokemons = createPokemons();
-
-	auto selectpokemons = SelectPokemon(pokemons);
-	while (true);
+	if (skillType == "Normal")
 	{
-		PrintBattle(selectpokemons.first, selectpokemons.second);
+		return 1;
 	}
-	return 0;
+	else if (skillType == "Ground")
+	{
+		if (pokemonType == "Normal" || pokemonType == "Ground" || pokemonType == "Fire")
+		{
+			return 1;
+		}
+		else if (pokemonType == "Electric" || pokemonType == "Grass")
+		{
+			return 0;
+		}
+		else
+		{
+			return 2;
+		}
+	}
+	else if (skillType == "Electric")
+	{
+		if (pokemonType == "Normal" || pokemonType == "Water" ||
+			pokemonType == "Grass" || pokemonType == "Fire")
+		{
+			return 1;
+		}
+		else if (pokemonType == "Electric")
+		{
+			return 0;
+		}
+		else
+		{
+			return 2;
+		}
+	}
+	else if (skillType == "Water")
+	{
+		if (pokemonType == "Normal" || pokemonType == "Ground")
+		{
+			return 1;
+		}
+		else if (pokemonType == "Water" || pokemonType == "Fire")
+		{
+			return 0;
+		}
+		else
+		{
+			return 2;
+		}
+	}
+	else if (skillType == "Grass")
+	{
+		if (pokemonType == "Normal")
+		{
+			return 1;
+		}
+		else if (pokemonType == "Fire")
+		{
+			return 2;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	else if (skillType == "Fire")
+	{
+		if (pokemonType == "Normal" || pokemonType == "Electric")
+		{
+			return 1;
+		}
+		else if (pokemonType == "Grass" || pokemonType == "Fire")
+		{
+			return 0;
+		}
+		else
+		{
+			return 2;
+		}
+	}
+}
+
+
+
+int main()
+{
+	bool isRunning = true;
+	while (isRunning)
+	{
+
+	}
 }
