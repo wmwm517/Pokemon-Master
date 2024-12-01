fd54ea7da7f492ddc0f171432412fe0f8c70c864
wmwm517 <apple051717@gmail.com>
2024-12-01 16:54:39 +0900

    feat: 화면 그리기

--- pokemon/pokemon.cpp
+++ pokemon/pokemon.cpp
@@ -15,10 +15,12 @@ public:
 	bool isLive();
 	bool canTry(int Skillnum);
 	int effect(string skillType, string pokemonType);
+	void latestSkill(); //쫌이따짜기
 protected:
 	string name;
 	string type;
 	string lastskill;
+	string lasteffect;
 	int HP;
 	int Skillcnt[4];
 };
@@ -135,6 +137,52 @@ int main()
 	bool isRunning = true;
 	while (isRunning)
 	{
+		int num1, num2;
+		cout << "Choose a Pokemon(0~4): ";
+		cin >> num1;
+		cout << "Choose a Pokemon(0~4): ";
+		cin >> num2;
+
+		if (num2 == num1);
+		{
+			cout << "You have to choose Pokemons different from each other.";
+			return 0;
+		}	
+
+		Pokemon p1, p2;
+
+		for (int i=0; i<2; i++)
+		{
+			cout << "+-------------------------------------------------------------+" << endl;
+			cout << "| 2024-02 Object-Oriented Programming Pokemon Master          |" << endl;
+			cout << "+------------------------------+------------------------------+" << endl;
+			cout << "| Pikachu (*)                  | Dratini                      |" << endl;
+			cout << "| Type: Electric               | Type: Water                  |" << endl;
+			cout << "| HP: 35                       | HP: 41                       |" << endl;
+			cout << "+------------------------------+------------------------------+" << endl;
+			cout << "| Latest Skill: -              | Latest Skill: -              |" << endl;
+			cout << "|                              |                              |" << endl;
+			cout << "+------------------------------+------------------------------+" << endl;
+			cout << "| (0) Tackle                   | (0) Wrap                     |" << endl;
+			cout << "|     - Type: Normal           |     - Type: Normal           |" << endl;
+			cout << "|     - Damage: 4              |     - Damage: 4              |" << endl;
+			cout << "|     - Count: 5(5)            |     - Count: 10(10)          |" << endl;
+			cout << "| (1) Grass Knot               | (1) Aqua Tail                |" << endl;
+			cout << "|     - Type: Grass            |     - Type: Water            |" << endl;
+			cout << "|     - Damage: 8              |     - Damage: 3              |" << endl;
+			cout << "|     - Count: 5(5)            |     - Count: 5(5)            |" << endl;
+			cout << "| (2) Thunderbolt              | (2) Water Pulse              |" << endl;
+			cout << "|     - Type: Electric         |     - Type: Water            |" << endl;
+			cout << "|     - Damage: 10             |     - Damage: 13             |" << endl;
+			cout << "|     - Count: 5(5)            |     - Count: 2(2)            |" << endl;
+			cout << "| (3) Megabolt                 | (3) Hyper Beam               |" << endl;
+			cout << "|     - Type: Electric         |     - Type: Normal           |" << endl;
+			cout << "|     - Damage: 15             |     - Damage: 20             |" << endl;
+			cout << "|     - Count: 3(3)            |     - Count: 1(1)            |" << endl;
+			cout << "+------------------------------+------------------------------+" << endl;
+			cout << "Choose a skill (0~3): ";	
+		}
+
 		break;
 	}
 }
